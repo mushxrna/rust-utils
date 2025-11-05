5beab795bf584c275e10e657fbc05eb2eaf3fdb4
mushxrna <0gage.zeidler@gmail.com>
2025-11-05 16:10:49 -0500

    general changes

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -8,5 +8,6 @@ mod string_buffer;
 pub use byte_package::BytePackage;
 pub use errors::ParseError;
 pub use literal::Literal;
+pub use literal::Operand;
 pub use optable::OpTable;
 pub use string_buffer::StringBuffer;
