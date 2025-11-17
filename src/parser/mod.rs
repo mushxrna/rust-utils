63232db9ca70cc904c086557137aa7ad21d9dd6e
mushxrna <0gage.zeidler@gmail.com>
2025-11-17 11:21:00 -0500

    working on ptrs

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -6,7 +6,7 @@ mod parser;
 mod string_buffer;
 
 pub use errors::ParseError;
-pub use heap::{ByteHeap, BytePointer};
+pub use heap::{ByteHeap, BytePointer, BytePtr};
 pub use literal::Literal;
 pub use literal::Operand;
 pub use optable::OpTable;
