26110d84299478c2d94b0d6d409a0a3fc7e898b3
mushxrna <0gage.zeidler@gmail.com>
2025-12-01 12:02:15 -0500

    added refmatchrule to exports

--- src/parsing/rules/mod.rs
+++ src/parsing/rules/mod.rs
@@ -1,5 +1,5 @@
 mod match_rules;
 mod rule_traits;
 
-pub use match_rules::{MatchRule, MatchRuleSet};
+pub use match_rules::{MatchRule, MatchRuleSet, RefMatchRule, RefMatchRuleSet};
 pub use rule_traits::{Rule, RuleSet};
