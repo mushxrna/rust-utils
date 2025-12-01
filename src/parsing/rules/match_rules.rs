6aea52aefd8f83549a3fb4577c32fc794b23b09b
mushxrna <0gage.zeidler@gmail.com>
2025-12-01 12:00:09 -0500

    tidied up match_rules and added a RefMatchRule with lifetime parameter

--- src/parsing/rules/match_rules.rs
+++ src/parsing/rules/match_rules.rs
@@ -2,37 +2,54 @@ use crate::parsing::rules::{Rule, RuleSet, match_rules};
 use std::any::Any;
 use std::borrow::Borrow;
 
-pub struct MatchRule<T: ?Sized, Z> {
-    pub rule: Box<dyn for<'a> Fn(&'a T) -> Option<Z>>,
+//
+// STRUCTS
+//
+pub struct MatchRule<Item: ?Sized, Result> {
+    pub rule: Box<dyn Fn(&Item) -> Option<Result>>,
     pub priority: usize,
 }
 
-pub struct MatchRuleSet<T: ?Sized, Z> {
-    pub match_rules: Vec<MatchRule<T, Z>>,
+pub struct MatchRuleSet<Item: ?Sized, Result> {
+    pub match_rules: Vec<MatchRule<Item, Result>>,
 }
 
-impl<T: ?Sized, Z> Default for MatchRuleSet<T, Z> {
-    fn default() -> Self {
-        Self {
-            match_rules: vec![],
-        }
-    }
+pub struct RefMatchRule<'rule, Input: ?Sized, Result> {
+    pub rule: Box<dyn Fn(&Input) -> Option<Result> + 'rule>,
+    pub priority: usize,
 }
 
-impl<T: ?Sized, Z> Rule for MatchRule<T, Z> {
-    type Item = T;
-    type Result = Option<Z>;
+pub struct RefMatchRuleSet<'rule, Input: ?Sized, Result> {
+    pub match_rules: Vec<RefMatchRule<'rule, Input, Result>>,
+}
+//
+// IMPL RULE
+//
+impl<A: ?Sized, B> Rule for MatchRule<A, B> {
+    type Item = A;
+    type Result = Option<B>;
 
     fn test<F: Borrow<Self::Item>>(&self, eval: &F) -> Self::Result {
         (self.rule)(eval.borrow())
     }
 }
 
-impl<T: ?Sized, Z> RuleSet for MatchRuleSet<T, Z> {
-    type Item = T;
-    type Result = Option<Z>;
+impl<A: ?Sized, B> Rule for RefMatchRule<'_, A, B> {
+    type Item = A;
+    type Result = Option<B>;
 
-    type Rule = MatchRule<T, Z>;
+    fn test<F: Borrow<Self::Item>>(&self, eval: &F) -> Self::Result {
+        (self.rule)(eval.borrow())
+    }
+}
+//
+// IMPL RULESET
+//
+impl<A: ?Sized, B> RuleSet for MatchRuleSet<A, B> {
+    type Item = A;
+    type Result = Option<B>;
+
+    type Rule = MatchRule<A, B>;
 
     fn get_rules(&self) -> &Vec<Self::Rule> {
         &self.match_rules
@@ -44,13 +61,61 @@ impl<T: ?Sized, Z> RuleSet for MatchRuleSet<T, Z> {
     }
 }
 
-impl<T: ?Sized, Z> MatchRule<T, Z> {
-    pub fn new(rule: Box<dyn Fn(&T) -> Option<Z>>, priority: usize) -> MatchRule<T, Z> {
+impl<'rule, A: ?Sized, B> RuleSet for RefMatchRuleSet<'rule, A, B> {
+    type Item = A;
+    type Result = Option<B>;
+
+    type Rule = RefMatchRule<'rule, A, B>;
+
+    fn get_rules(&self) -> &Vec<Self::Rule> {
+        &self.match_rules
+    }
+
+    fn insert(&mut self, rule: Self::Rule) {
+        self.match_rules.push(rule);
+        self.priority_sort();
+    }
+}
+//
+// IMPL DEFAULT
+//
+impl<A: ?Sized, B> Default for MatchRuleSet<A, B> {
+    fn default() -> Self {
+        Self {
+            match_rules: vec![],
+        }
+    }
+}
+impl<A: ?Sized, B> Default for RefMatchRuleSet<'_, A, B> {
+    fn default() -> Self {
+        Self {
+            match_rules: vec![],
+        }
+    }
+}
+//
+// IMPL METHODS
+//
+impl<A: ?Sized, B> MatchRule<A, B> {
+    pub fn new(rule: Box<dyn Fn(&A) -> Option<B>>, priority: usize) -> MatchRule<A, B> {
         MatchRule { rule, priority }
     }
 }
 
-impl<T: ?Sized, Z> MatchRuleSet<T, Z> {
+impl<A: ?Sized, B> MatchRuleSet<A, B> {
+    fn priority_sort(&mut self) {
+        self.match_rules
+            .sort_by_key(|rule| std::cmp::Reverse(rule.priority));
+    }
+}
+
+impl<'rule, A: ?Sized, B> RefMatchRule<'rule, A, B> {
+    pub fn new(rule: Box<dyn Fn(&A) -> Option<B> + 'rule>, priority: usize) -> RefMatchRule<A, B> {
+        RefMatchRule { rule, priority }
+    }
+}
+
+impl<T: ?Sized, Z> RefMatchRuleSet<'_, T, Z> {
     fn priority_sort(&mut self) {
         self.match_rules
             .sort_by_key(|rule| std::cmp::Reverse(rule.priority));
