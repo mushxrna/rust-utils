827effe90aefabd02ce8de34ccede5e45d7a53b1
mushxrna <0gage.zeidler@gmail.com>
2025-12-03 11:57:30 -0500

    added rulemap

--- src/parsing/rules/mod.rs
+++ src/parsing/rules/mod.rs
@@ -3,4 +3,5 @@ mod rule_map;
 mod rule_traits;
 
 pub use match_rules::{MatchRule, MatchRuleSet, RefMatchRule, RefMatchRuleSet};
+pub use rule_map::{ArgRule, RuleMap};
 pub use rule_traits::{Rule, RuleSet};
