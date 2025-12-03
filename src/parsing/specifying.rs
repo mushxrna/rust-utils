b56f8a8f426a5665053b78914731f71dcd4c59da
mushxrna <0gage.zeidler@gmail.com>
2025-12-02 23:26:30 -0500

    added specify ref to Specifier

--- src/parsing/specifying.rs
+++ src/parsing/specifying.rs
@@ -1,16 +1,19 @@
 use crate::parsing::rules::*;
-use std::borrow::Borrow;
+use std::{borrow::Borrow, fmt::Result};
 
 pub struct Specifier<T: RuleSet> {
     rule_set: T,
 }
 
-impl<I, T: RuleSet<Result = Option<I>>> Specifier<T> {
+impl<T: RuleSet> Specifier<T> {
     pub fn new(ruleset: T) -> Specifier<T> {
         Specifier { rule_set: ruleset }
     }
 
-    pub fn specify_sliced<R: Borrow<T::Item>>(&self, slice: &[R]) -> Vec<I> {
+    pub fn specify_sliced<I, R: Borrow<T::Item>>(&self, slice: &[R]) -> Vec<I>
+    where
+        T: RuleSet<Result = Option<I>>,
+    {
         let mut result = vec![];
 
         for i in slice.iter() {
@@ -26,5 +29,16 @@ impl<I, T: RuleSet<Result = Option<I>>> Specifier<T> {
         }
 
         result
+    }
+
+    pub fn specify_ref<I>(&self, item: &T::Item) -> I
+    where
+        T: RuleSet<Result = Option<I>>,
+    {
+        self.rule_set
+            .test_all(item)
+            .into_iter()
+            .find_map(|x| x)
+            .unwrap()
     }
 }
