805bd7f2ed01a8b3d6168d5d5f17805a75346313
mushxrna <0gage.zeidler@gmail.com>
2025-12-03 12:30:48 -0500

    added rulemap

--- src/parsing/rules/rule_map.rs
+++ src/parsing/rules/rule_map.rs
@@ -4,7 +4,7 @@ use std::collections::HashMap;
 use std::hash::Hash;
 
 pub struct ArgRule<Item, Result> {
-    rule: Box<dyn Fn(&Item, &[Item]) -> Result>,
+    rule: Box<dyn Fn(&[Item]) -> Result>,
 }
 
 pub struct RuleMap<B, R> {
@@ -18,11 +18,11 @@ impl<A: std::cmp::Eq + Hash, R> RuleMap<A, R> {
         }
     }
 
-    pub fn insert(&mut self, item: A, rule: Box<dyn Fn(&A, &[A]) -> R>) {
+    pub fn insert(&mut self, item: A, rule: Box<dyn Fn(&[A]) -> R>) {
         self.map.insert(item, ArgRule { rule: rule });
     }
 
     pub fn evaluate(self, item: A, args: &[A]) -> Option<R> {
-        Some((self.map.get(&item)?.rule)(&item, args))
+        Some((self.map.get(&item)?.rule)(args))
     }
 }
