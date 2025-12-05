991e4f653eb0e00172c5ea2bbd5c2e3298471bb1
mushxrna <0gage.zeidler@gmail.com>
2025-12-05 14:31:26 -0500

    reworking nester (tree)

--- src/parsing/nesting.rs
+++ src/parsing/nesting.rs
@@ -11,10 +11,6 @@ pub struct IndexNode {
     index: Option<usize>,             // points to an index in a SPLIT source.
     children: Option<Vec<IndexNode>>, //child nodes.
 }
-pub struct IndexTree {
-    nodes: Vec<IndexNode>,
-    pub root: IndexNode,
-}
 pub struct Nester<A: Molecule> {
     pub delimiters: (A, A),
 }
@@ -52,6 +48,10 @@ impl IndexNode {
     pub fn get_index_or_panic(&self) -> usize {
         self.index
             .expect("PANIC! Used get_index_or_panic on IndexNode that does not have an index.")
+    }
+
+    pub fn collapse(self) -> Option<Vec<IndexNode>> {
+        self.children
     }
 }
 
@@ -102,94 +102,6 @@ impl<A: Molecule> Nester<A> {
             children: Some(node_pool),
         }
     }
-
-    /*
-
-    fn _nest_tree_recursive<CompA>(&self, source: &[CompA], offset: usize) -> IndexTree
-    where
-        CompA: AsRef<[A::Atom]>,
-    {
-        let source_vec: Vec<&CompA> = source.iter().collect();
-
-        let mut result = vec![];
-
-        let mut index = 0;
-
-        while index < source.len() {
-            let value = &source[index];
-
-            if value.as_ref() == &*self.delimiters.0 {
-                let mut possible_range = source[index..].iter();
-                let mut delimiters_found = 0;
-                let mut distance = 0;
-
-                while let Some(item) = possible_range.next() {
-                    if item.as_ref() == &*self.delimiters.0 {
-                        delimiters_found += 1;
-                    } else if item.as_ref() == &*self.delimiters.1 {
-                        delimiters_found -= 1;
-                    }
-
-                    if delimiters_found == 0 {
-                        break;
-                    }
-
-                    distance += 1;
-                }
-
-                let (start, end) = (index + 1, index + distance);
-
-                let evaluated_inner = self
-                    ._nest_tree_recursive(&source[start..end], offset + start)
-                    .release_nodes();
-
-                let parent_index = result.len();
-                let children_start = parent_index + 1;
-                let children_end = children_start + evaluated_inner.len();
-
-                result.push(IndexNode {
-                    range: ((offset + start)..(offset + end + 1)),
-                    children: if evaluated_inner.is_empty() {
-                        None
-                    } else {
-                        Some(children_start..children_end)
-                    },
-                });
-
-                // Adjust child indices in evaluated_inner to account for the offset
-                let index_offset = children_start;
-                let adjusted_inner: Vec<IndexNode> = evaluated_inner
-                    .into_iter()
-                    .map(|mut node| {
-                        if let Some(x) = node.children {
-                            node.children = Some(x.start + index_offset..x.end + index_offset);
-                        }
-                        node
-                    })
-                    .collect();
-
-                result.extend(adjusted_inner);
-                index = end + 1;
-            } else {
-                result.push(IndexNode {
-                    range: ((offset + index)..(offset + index + 1)),
-                    children: None,
-                });
-                index += 1;
-            }
-        }
-
-        let l = result.len();
-
-        IndexTree {
-            nodes: result,
-            root: IndexNode {
-                range: 0..0,
-                children: Some(0..l),
-            },
-        }
-    }
-    */
 }
 //
 // IMPL DISPLAY
@@ -205,6 +117,14 @@ impl<T: std::fmt::Display> std::fmt::Display for NestedObject<T> {
                 }
                 write!(f, "Molecule: ( {s} )")
             }
+        }
+    }
+}
+impl std::fmt::Display for IndexNode {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+        match self.get_children() {
+            Some(c) => write!(f, "Parent Node! Owner of {} nodes.", c.len()),
+            None => write!(f, "Child Node!"),
         }
     }
 }
