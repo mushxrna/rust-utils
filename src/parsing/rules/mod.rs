616f51bd181952f3bb93ad53218f6b7aadcee436
mushxrna <0gage.zeidler@gmail.com>
2025-12-08 16:10:43 -0500

    .

--- src/parsing/rules/mod.rs
+++ src/parsing/rules/mod.rs
@@ -2,6 +2,6 @@ mod match_rules;
 mod rule_map;
 mod rule_traits;
 
-pub use match_rules::{MatchRule, MatchRuleResult, MatchRuleSet};
+pub use match_rules::{MatchFn, MatchRule, MatchRuleResult, MatchRuleSet};
 pub use rule_map::{ArgRule, RuleMap};
 pub use rule_traits::{Rule, RuleSet};
