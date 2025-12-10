dcd38d98896bb178a03ee2918f2e2f5f5f2700fe
mushxrna <0gage.zeidler@gmail.com>
2025-12-10 16:05:41 -0500

    starting to work on some other things besides parsing...

---
+++ src/datastructures/byte_heap.rs
@@ -0,0 +1,61 @@
+use thiserror::Error;
+
+use crate::generics::Byteable;
+
+const MANAGED_FLAG: u8 = 0b1000_0000;
+
+macro_rules! flag_check {
+    ($flag:expr, $item:expr) => {
+        ($item & $flag == $flag)
+    };
+}
+
+#[derive(Debug, Error)]
+pub enum ByteHeapError {
+    #[error("Could not insert item.")]
+    InsertError,
+    #[error("No avalaible space in the heap.")]
+    AllocError,
+}
+
+use ByteHeapError as E;
+
+pub struct ByteHeap<const SIZE: usize> {
+    bytes: Box<[u8; SIZE]>,
+    allocation_flags: Box<[u8; SIZE]>,
+}
+
+impl<const S: usize> ByteHeap<S> {
+    fn find_available(&self, space_in_bytes: usize) -> Result<usize, E> {
+        let mut consecutive = 0;
+        let mut start = 0;
+
+        for (i) in 0..self.bytes.len() {
+            if !flag_check!(self.allocation_flags[i], MANAGED_FLAG) {
+                consecutive += 1;
+            }
+
+            if flag_check!(self.allocation_flags[i], MANAGED_FLAG) {
+                consecutive = 0;
+            }
+
+            if consecutive == space_in_bytes {
+                return Ok(i - consecutive);
+            }
+        }
+
+        Err(E::AllocError)
+    }
+
+    pub fn insert<A: Byteable>(&mut self, item: A) -> Result<usize, E> {
+        let bytes = item.to_bytes();
+        let index = self.find_available(bytes.len())?;
+
+        let insertion_region = self
+            .bytes
+            .get_mut(index..index + bytes.len())
+            .ok_or(E::InsertError)?;
+
+        Ok(10)
+    }
+}
--- src/datastructures/mod.rs
+++ src/datastructures/mod.rs
@@ -1,3 +1,5 @@
+mod byte_heap;
 mod index_tree;
 
+pub use byte_heap::ByteHeap;
 pub use index_tree::IndexNode;
