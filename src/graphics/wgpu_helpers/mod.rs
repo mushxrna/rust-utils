d99a961c1088a4cd1e7aa6504932436436d8bdff
mushxrna <0gage.zeidler@gmail.com>
2026-01-02 23:50:37 -0500

    purged wgpu_helpers.

--- src/graphics/wgpu_helpers/mod.rs
+++ src/graphics/wgpu_helpers/mod.rs
@@ -5,20 +5,6 @@ pub mod common {
     };
 }
 
-//mod config_enums;
-mod errors;
-//mod high_level_types;
-//mod pod_types;
-
-//mod buffermanager;
 mod contextmanager;
-//mod pipelinemanager;
-//mod texturemanager;
-
-//pub use buffermanager::BufferManager;
-//pub use config_enums::*;
+mod errors;
 pub use contextmanager::WgpuContextManager;
-//pub use high_level_types::*;
-//pub use pipelinemanager::PipelineManager;
-//pub use pod_types::*;
-//pub use texturemanager::TextureManager;
